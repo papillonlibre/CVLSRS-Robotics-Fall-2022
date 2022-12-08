5ddf56f0ed5f88e59deb7005e0fcfe1e89ea2bd9
papillonlibre <bellottivanessa@gmail.com>
2022-12-07 20:23:55 -0500

    added tilting code and control logic

--- detection_handling.py
+++ detection_handling.py
@@ -11,7 +11,7 @@ import cv2
 from camera_reader import CameraFeed
 import numpy as np
 from emitter import Emitter
-from messages import MsgType, message_buffers, RotationCommand
+from messages import MsgType, message_buffers, RotationCommand, TiltCommand
 
 # retrieving address and port of robomodules server (from env vars)
 ADDRESS = os.environ.get("LOCAL_ADDRESS","localhost")
@@ -23,6 +23,7 @@ START_POS = -1
 
 e = Emitter()
 cf = CameraFeed(0)
+msg3 = TiltCommand()
 
 
 class ShapeHandling(rm.ProtoModule):
@@ -143,7 +144,7 @@ class ShapeHandling(rm.ProtoModule):
       # # Showing the output
       
     
-    def find_triangles(self, image, START_POS):
+    def find_triangles(self, image, START_POS, msg3):
       print("finding triangles")
       # finding contours (edges of shapes) in image
       edges = cv2.Canny(image, 30, 200)
@@ -160,18 +161,20 @@ class ShapeHandling(rm.ProtoModule):
           triangle_count += 1
           e.send_pulse()
           cv2.drawContours(output_image, [approx], -1, (255, 0, 0), 3)
-        if (triangle_count == 0):
+        if (triangle_count == 0 and msg3.position == 1): # when target not detected && tilt not finished
           print("no triangles")
           msg = RotationCommand()
           START_POS += .1
           msg.position = START_POS
           msg.max_speed = 1
           self.write(msg.SerializeToString(), MsgType.ROTATION_COMMAND)
+          msg3.position = -1 # reset tilt value
+          self.write(msg3.SerializeToString(), MsgType.TILT_COMMAND)
       # cv2.imshow("Output", output_image)
       # if cv2.waitKey(1) & 0xFF == ord('q'):
       #       break
       return output_image, triangle_count
-    def find_squares(self, image, START_POS):
+    def find_squares(self, image, START_POS, msg3):
       print("FIND SQUARES WAS CALLED")
       # finding contours (edges of shapes) in image
       edges = cv2.Canny(image, 30, 200)
@@ -188,20 +191,22 @@ class ShapeHandling(rm.ProtoModule):
           print("square found")
           e.send_pulse()
           cv2.drawContours(output_image, [approx], -1, (255, 0, 0), 3)
-        if (square_count == 0):
+        if (square_count == 0 and msg3.position == 1): # when target not detected && tilt not finished
           print("no squares")
           msg = RotationCommand()
           START_POS += .1
           msg.position = START_POS
           msg.max_speed = 1
           self.write(msg.SerializeToString(), MsgType.ROTATION_COMMAND)
+          msg3.position = -1 # reset tilt value
+          self.write(msg3.SerializeToString(), MsgType.TILT_COMMAND)
           print ("square not found")
         # print("DEBUGGING SQUARE_COUNT", square_count)
       # cv2.imshow("Output", output_image)
       # cv2.waitKey(0)
       return output_image, square_count
   
-    def find_octagons(self, image, START_POS):
+    def find_octagons(self, image, START_POS, msg3):
       print("finding octagons")
       # finding contours (edges of shapes) in image
       edges = cv2.Canny(image, 30, 200)
@@ -218,18 +223,20 @@ class ShapeHandling(rm.ProtoModule):
           octagons_count += 1
           e.send_pulse()
           cv2.drawContours(output_image, [approx], -1, (255, 0, 0), 3)
-        if (octagons_count == 0):
+        if (octagons_count == 0 and msg3.position == 1): # when target not detected && tilt not finished
           print("no octagon found")
           msg = RotationCommand()
           START_POS += .1
           msg.position = START_POS
           msg.max_speed = 1
           self.write(msg.SerializeToString(), MsgType.ROTATION_COMMAND)
+          msg3.position = -1 # reset tilt value
+          self.write(msg3.SerializeToString(), MsgType.TILT_COMMAND)
       # cv2.imshow("Output", output_image)
       # cv2.waitKey(0)
       return output_image, octagons_count
   
-    def find_circles(self, image, START_POS):
+    def find_circles(self, image, START_POS, msg3):
       # finding contours (edges of shapes) in image
       print("looking for circles")
       edges = cv2.Canny(image, 30, 200)
@@ -247,13 +254,15 @@ class ShapeHandling(rm.ProtoModule):
           cv2.drawContours(output_image, [approx], -1, (255, 0, 0), 3)
           e.send_pulse(0)
         
-        if (circle_count == 0):
+        if (circle_count == 0 and msg3.position == 1): # when target not detected && tilt not finished
           print("no circle found")
-          msg = RotationCommand()
+          msg_motor = RotationCommand()
           START_POS += .1
-          msg.position = START_POS
-          msg.max_speed = 1
-          self.write(msg.SerializeToString(), MsgType.ROTATION_COMMAND)
+          msg_motor.position = START_POS
+          msg_motor.max_speed = 1
+          self.write(msg_motor.SerializeToString(), MsgType.ROTATION_COMMAND)
+          msg3.position = -1 # reset tilt value
+          self.write(msg3.SerializeToString(), MsgType.TILT_COMMAND)
       # cv2.imshow("Output", output_image)
       # cv2.waitKey(0)
       return output_image, circle_count
@@ -264,6 +273,9 @@ class ShapeHandling(rm.ProtoModule):
         msg2.position = START_POS
         msg2.max_speed = 1
         self.write(msg2.SerializeToString(), MsgType.ROTATION_COMMAND)
+
+        msg3.position = START_POS
+        self.write(msg3.SerializeToString(), MsgType.ROTATION_COMMAND)
 
         
         if (msg_type == MsgType.TARGET):
@@ -279,21 +291,25 @@ class ShapeHandling(rm.ProtoModule):
             # print ("green was found")
             cf2 = self.find_green(cf.read())
           
-          # color detection on the new masked camera feed frame read
-          if (msg.shape == 0):
-            print("looking for squares")
-            output,count = self.find_squares(cf2, START_POS)
-          elif (msg.shape == 1):
-            output,count =self.find_circles(cf2, START_POS)
-          elif (msg.shape == 2):
-            output,count =self.find_triangles(cf2, START_POS)
-          elif (msg.shape == 3):
-            output,count = self.find_octagons(cf2, START_POS)
-          while True:
-            cv2.imshow("testing", output)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-            # sys.exit() # to exit from all the processes
+
+          while (msg3.position != 1): # need to tilt to achieve full view
+            # color detection on the new masked camera feed frame read
+            if (msg.shape == 0):
+              print("looking for squares")
+              output,count = self.find_squares(cf2, START_POS, msg3)
+            elif (msg.shape == 1):
+              output,count =self.find_circles(cf2, START_POS, msg3)
+            elif (msg.shape == 2):
+              output,count =self.find_triangles(cf2, START_POS, msg3)
+            elif (msg.shape == 3):
+              output,count = self.find_octagons(cf2, START_POS, msg3)
+            while True:
+              cv2.imshow("testing", output)
+              if cv2.waitKey(1) & 0xFF == ord('q'):
+                  break
+              # sys.exit() # to exit from all the processes
+            msg3.position += .1
+            self.write(msg3.SerializeToString(), MsgType.ROTATION_COMMAND)
  
           cv2.destroyAllWindows() # destroy all windows
 
