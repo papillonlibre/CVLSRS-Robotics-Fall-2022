c43de36d1de05ea7468c8e22aa1811cb3b526471
Alexander Koppel <32113635+alko08@users.noreply.github.com>
2023-12-08 16:22:45 -0500

    Updated Shape and Color Handling
    Added the option for color only. ONLYCOLOR is set to true by default.
    
    Also cleaned up code overall and speeded up movement to remove delay.

--- detectionHandler.py
+++ detectionHandler.py
@@ -12,9 +12,10 @@ from local_camera_reader import LocalCameraFeed
 # Retrieving address and port of robomodules server (from env vars)
 ADDRESS = os.environ.get("LOCAL_ADDRESS", "localhost")
 PORT = os.environ.get("LOCAL_PORT", 11295)
-FREQUENCY = 3
+FREQUENCY = 5
 
-TESTING = False
+TESTING = True
+ONLYCOLOR = True
 
 class ShapeHandling(rm.ProtoModule):
     def __init__(self, addr, port):
@@ -26,17 +27,13 @@ class ShapeHandling(rm.ProtoModule):
         self.msg3 = TiltCommand()
 
         self.targetSearching = False
-        self.foundShape = False
         self.tilt = 0
         self.up = True
+        self.lastSeen = 0
         self.color = -1
         self.shape = -1
 
     def tick(self):
-        self.move()
-        print(f"The color is {self.color}")
-        # return self.move()
-
         # control logic for detecting colors
         if self.color == 0: # Red
             cf2 = self.find_color(self.cf.read(), np.array([0, 100, 100]), np.array([15, 255, 255]))
@@ -47,27 +44,32 @@ class ShapeHandling(rm.ProtoModule):
         elif self.color == 3: # Green
             cf2 = self.find_color(self.cf.read(), np.array([70, 100, 100]), np.array([85, 175, 255]))
         else: # Not a valid color
+            print("Not given a valid color. Waiting for signal...")
             return
-
-        if self.shape == 0: # Square
-            output, _ = self.find_squares(cf2)
-        elif self.shape == 1: # Circle
-            output, _ = self.find_circles(cf2)
-        elif self.shape == 2: # Triangle
-            output, _ = self.find_triangles(cf2)
-        elif self.shape == 3: # Octagon
-            output, _ = self.find_octagons(cf2)
-        else: # Not a valid shape
-            return
+        
+        if ONLYCOLOR:
+            self.find_close(cf2)
+        else:
+            if self.shape == 0: # Square
+                self.find_squares(cf2)
+            elif self.shape == 1: # Circle
+                self.find_circles(cf2)
+            elif self.shape == 2: # Triangle
+                self.find_triangles(cf2)
+            elif self.shape == 3: # Octagon
+                self.find_octagons(cf2)
+            else: # Not a valid shape
+                return
         
         if TESTING:
-            cv2.imshow("Testing", output)
-        #     if cv2.waitKey(1) & 0xFF == ord('q'):
-        #         break
-        
-        # if TESTING:
-        #     cv2.destroyAllWindows()
-        return
+            color = ["Red", "Yellow", "Blue", "Green"]
+            shape = ["Square", "Circle", "Triangle", "Octagon"]
+            if ONLYCOLOR:
+                print(f'Searching for: {color[self.color]}')
+            else:
+                print(f'Searching for: {color[self.color]} {shape[self.shape]}')
+        return self.move()
+
 
     def find_color(self, image, lower_bound, upper_bound):
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -80,69 +82,107 @@ class ShapeHandling(rm.ProtoModule):
         output = cv2.drawContours(segmented_img, contours, -1, (0, 0, 255), 3)
         return output
 
-    def find_shapes(self, image, shape_name, target_count):
+
+    def find_close(self, image):
         edges = cv2.Canny(image, 30, 200)
         contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        # contours = imutils.grab_contours(contours)
-
-
-        output_image = image.copy()
-        count = 0
-        self.foundShape = False
+        self.lastSeen += 1
+        foundShape = False
+        cXs = []
+        cYs = []
 
         for contour in contours:
+            if cv2.contourArea(contour) < 20:
+                continue
+            
+            M = cv2.moments(contour)
+            if (M["m00"] == 0):
+                continue
+            cX = int(M["m10"]/ M["m00"])
+            cY = int(M["m01"]/ M["m00"])
+
+            cXs.append(cX)
+            cYs.append(cY)
+            foundShape = True
+
+        self.targetSearching = not foundShape
+        if foundShape:
+            self.lastSeen = 0
+            cX_av = sum(cXs) / len(cXs) 
+            cY_av = sum(cYs) / len(cYs) 
+
+            if not (220 < cY_av < 260):
+                self.tilt += (240 - cY_av) / 480
+            
+            if not (300 < cX_av < 340):
+                rotation += (cY_av - 320) / 15
+            else:
+                rotation = 0
+            
+            if ((220 < cY < 260) and (300 < cX < 340)):
+                msg = LaserCommand()
+                msg.seconds = 1 / FREQUENCY
+                self.write(msg.SerializeToString(), MsgType.LASER_COMMAND)
+
+            self.move_check(rotation)
+        return
+
+
+    def find_shapes(self, image, target_count):
+        edges = cv2.Canny(image, 30, 200)
+        contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        self.lastSeen += 1
+        foundShape = False
+        cXs = []
+        cYs = []
+
+        for contour in contours:
+            if cv2.contourArea(contour) < 20:
+                continue
+            
             M = cv2.moments(contour)
             if (M["m00"] == 0):
                 continue
             cX = int(M["m10"]/ M["m00"])
             cY = int(M["m01"]/ M["m00"])
             
-
-            # print(f"The cX is {cX} and the cY is {cY}\n")
-
             approx = cv2.approxPolyDP(contour, 1.95, True)
             if len(approx) == target_count:
-                count += 1
-                seconds = 2
-                msg = LaserCommand()
-                msg.seconds = seconds
-                self.targetSearching = False
-                self.foundShape = True
-                
-                cv2.drawContours(output_image, [approx], -1, (255, 0, 0), 3)
-        
-        if not self.foundShape:
-            self.targetSearching = True
-        else:
-            if (cY < 220):
-                self.tilt += .3
-            elif (cY > 260):
-                self.tilt -= .3
+                cXs.append(cX)
+                cYs.append(cY)
+                foundShape = True
 
-            if (cX < 300):
-                self.msg2.position = -10
-            elif (cX > 340):
-                self.msg2.position = 10
+        self.targetSearching = not foundShape
+        if foundShape:
+            self.lastSeen = 0
+            cX_av = sum(cXs) / len(cXs) 
+            cY_av = sum(cYs) / len(cYs) 
+
+            if not (220 < cY_av < 260):
+                self.tilt += (240 - cY_av) / 480
+            
+            if not (300 < cX_av < 340):
+                rotation += (cY_av - 320) / 15
+            else:
+                rotation = 0
             
             if ((220 < cY < 260) and (300 < cX < 340)):
+                msg = LaserCommand()
+                msg.seconds = 1 / FREQUENCY
                 self.write(msg.SerializeToString(), MsgType.LASER_COMMAND)
 
-            self.write(self.msg2.SerializeToString(), MsgType.ROTATION_COMMAND)
-            self.msg3.position = self.tilt
-            self.write(self.msg3.SerializeToString(), MsgType.TILT_COMMAND)
-        return output_image, count
+            self.move_check(rotation)
+        return
+
 
     def find_triangles(self, image):
-        print("Finding triangles")
-        return self.find_shapes(image, "Triangles", 3)
+        return self.find_shapes(image, 3)
 
     def find_squares(self, image):
-        print("Finding squares")
-        return self.find_shapes(image, "Squares", 4)
+        return self.find_shapes(image, 4)
 
     def find_octagons(self, image):
-        print("Finding octagons")
-        return self.find_shapes(image, "Octagons", 8)
+        return self.find_shapes(image, 8)
 
     def find_circles(self, image):
         # Convert the image to grayscale
@@ -150,20 +190,11 @@ class ShapeHandling(rm.ProtoModule):
         # Apply GaussianBlur to reduce noise and improve circle detection
         blurred = cv2.GaussianBlur(gray, (9, 9), 2)
         # Use Hough Circle Transform to detect circles
-        circles = cv2.HoughCircles(
-            blurred,
-            cv2.HOUGH_GRADIENT,
-            dp=1,
-            minDist=50,
-            param1=50,
-            param2=30,
-            minRadius=70,
-            maxRadius=90,
-        )
+        circles = cv2.HoughCircles( blurred, cv2.HOUGH_GRADIENT, dp=1,
+            minDist=50, param1=50, param2=30, minRadius=70, maxRadius=90)
 
         output_image = image.copy()
         circle_count = 0
-
         if circles is not None:
             circles = np.uint16(np.around(circles))
             for i in circles[0, :]:
@@ -177,9 +208,9 @@ class ShapeHandling(rm.ProtoModule):
 
         return output_image, circle_count
     
+
     def move(self):
-        # print(f"The tilt is {self.tilt}")
-        if self.targetSearching and not self.foundShape:
+        if self.targetSearching and self.lastSeen > 1:
             if self.up:
                 self.tilt += .5
             else:
@@ -191,24 +222,38 @@ class ShapeHandling(rm.ProtoModule):
                     self.tilt = .5
                 else:
                     self.tilt = -1
-                self.msg2.position = 15
+                self.msg2.position = 30
                 self.write(self.msg2.SerializeToString(), MsgType.ROTATION_COMMAND)
 
             self.msg3.position = self.tilt
             self.write(self.msg3.SerializeToString(), MsgType.TILT_COMMAND)
         return
+    
+    def move_check(self, rotation):
+        if self.tilt > .5:
+            self.tilt = .5
+        elif self.tilt < -1:
+            self.tilt < -1
+        
+        self.msg2.position = rotation
+        self.write(self.msg2.SerializeToString(), MsgType.ROTATION_COMMAND)
+
+        self.msg3.position = self.tilt
+        self.write(self.msg3.SerializeToString(), MsgType.TILT_COMMAND)
+
 
 
     def msg_received(self, msg, msg_type):
-        print("Message received!")
+        print("Message received!\n")
         if (msg_type == MsgType.TARGET):
             self.color = msg.color
             self.shape = msg.shape
             self.targetSearching = True
+            self.lastSeen = 0
 
 
 def main():
-    print("STARTING THE PI")
+    print("STARTING THE PI\n")
     module = ShapeHandling(ADDRESS, PORT)
     module.run()
 
